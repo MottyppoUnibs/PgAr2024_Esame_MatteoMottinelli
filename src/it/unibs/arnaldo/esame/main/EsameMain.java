199dcade311f7e8f6c554a700abb99d94421f9d3
Motty <m.mottinelli002@studenti.unibs.it>
2024-06-07 13:55:16 +0200

    implemented

--- src/it/unibs/arnaldo/esame/main/EsameMain.java
+++ src/it/unibs/arnaldo/esame/main/EsameMain.java
@@ -1,8 +1,31 @@
 package it.unibs.arnaldo.esame.main;
 
+import it.unibs.arnaldo.esame.game.Player;
+import it.unibs.arnaldo.esame.game.Saloon;
+import it.unibs.arnaldo.esame.io.Ranking;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //TODO: DOC
 public class EsameMain {
+
     public static void main(String[] args) {
-        //TODO: everything here
+        UserInteraction.welcome();
+        // choose mode
+        int mode = UserInteraction.chooseMode();
+        // choose and init players
+        int numberOfPlayers = UserInteraction.chooseNumberOfPlayers();
+        List<Player> players = new ArrayList<>();
+        for (int i = 0; i < numberOfPlayers; i++) {
+            players.add(new Player(UserInteraction.chooseName(i + 1), mode));
+        }
+        // let's start a gaming session
+        Saloon saloon = new Saloon(players, mode);
+        saloon.play();
+        // writing the rankings
+        Ranking ranking = new Ranking(saloon.getLobby());
+        ranking.writeRanking();
+        UserInteraction.farewell();
     }
 }
